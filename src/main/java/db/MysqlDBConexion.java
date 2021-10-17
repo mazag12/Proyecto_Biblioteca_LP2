4cf5d6837bc83f244da6a608882c6e5849d96ef8
mazag12 <maxter_anime12@outlook.com>
2021-10-17 13:22:54 -0500

    cambios
    sdadsa

--- src/main/java/db/MysqlDBConexion.java
+++ src/main/java/db/MysqlDBConexion.java
@@ -1,7 +1,33 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MysqlDBConexion {
 
-	
-	
+	public static Connection getConexion() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/BD_BIBLIOTECA";
+			String usr = "root";
+			String psw = "mysql";
+			con = DriverManager.getConnection(url, usr, psw);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error >> Driver no Instalado!!" + e.getMessage());
+		} catch (SQLException e) {
+			System.out.println("Error >> de conexión con la BD" + e.getMessage());
+		}
+		return con;
+	}
+
+	public static void closeConexion(Connection con) {
+
+		try {
+			con.close();
+		} catch (SQLException e) {
+			System.out.println("Problemas al cerrar la conexion");
+		}
+	}
 }
