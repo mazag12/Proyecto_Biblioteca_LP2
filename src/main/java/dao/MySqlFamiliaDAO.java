d62b7a4ea4fd0c32e049c6a6c58b964ddd6624f9
TAV0MACA <91575347+TAV0MACA@users.noreply.github.com>
2021-11-06 17:51:41 -0500

    Update MySqlFamiliaDAO.java

--- src/main/java/dao/MySqlFamiliaDAO.java
+++ src/main/java/dao/MySqlFamiliaDAO.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import db.MysqlDBConexion; 
+import db.MysqlDBConexion;
 
 import beans.Familia;
 import interfaces.FamiliaInterfaceDAO;
