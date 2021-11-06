43a7e536c1e251b2620ad6c9299a4fb502dbfb71
TAV0MACA <91575347+TAV0MACA@users.noreply.github.com>
2021-11-06 09:14:15 -0500

    Create Estudios.java

---
+++ src/main/java/beans/Estudios.java
@@ -0,0 +1,55 @@
+package entities;
+
+public class Estudios {
+
+	private String codEstudiante;
+	private String nomEstudio;
+	private String carrera;
+	private int telefono;
+	private int celular;
+	private String direccion;
+	private String codPais;
+	public String getCodEstudiante() {
+		return codEstudiante;
+	}
+	public void setCodEstudiante(String codEstudiante) {
+		this.codEstudiante = codEstudiante;
+	}
+	public String getNomEstudio() {
+		return nomEstudio;
+	}
+	public void setNomEstudio(String nomEstudio) {
+		this.nomEstudio = nomEstudio;
+	}
+	public String getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public int getCelular() {
+		return celular;
+	}
+	public void setCelular(int celular) {
+		this.celular = celular;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getCodPais() {
+		return codPais;
+	}
+	public void setCodPais(String codPais) {
+		this.codPais = codPais;
+	}
+	
+}
