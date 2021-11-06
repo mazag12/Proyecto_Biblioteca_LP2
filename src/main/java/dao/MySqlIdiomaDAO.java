cfda3438c52808fc61401406c3671538b25ec213
TAV0MACA <91575347+TAV0MACA@users.noreply.github.com>
2021-11-06 18:21:24 -0500

    Create MySqlIdiomaDAO.java

---
+++ src/main/java/dao/MySqlIdiomaDAO.java
@@ -0,0 +1,63 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.MySqlConexion;
+import beans.Idioma;
+import interfaces.IdiomaInterfaceDAO;
+
+public class MySqlIdiomaDAO implements IdiomaInterfaceDAO{
+
+	@Override
+	public List<Idioma> getListIdiomas() {
+		
+		List<Idioma> listidioma = new ArrayList<Idioma>();
+		
+		Connection cn =  null;
+		PreparedStatement psm = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			cn = MySqlConexion.getConexion();
+			
+			String sql = "select * from idioma";
+			
+			psm = cn.prepareStatement(sql);
+			rs = psm.executeQuery();
+			
+			while (rs.next()) {
+				
+				Idioma idio = new Idioma();
+				
+				idio.setCodIdioma(rs.getString("CODIDIOMA"));
+				idio.setNomIdioma(rs.getString("NOMIDIOMA"));
+
+				listidioma.add(idio);
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			try {
+				
+				if(rs != null) rs.close();
+				if(psm != null) psm.close();
+				if(cn != null) cn.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return listidioma;
+	}
+
+}
