121210b3bb8ae2cf5068285c991fd5ab0f6b6c8c
TAV0MACA <91575347+TAV0MACA@users.noreply.github.com>
2021-11-06 18:17:26 -0500

    Create Idioma.java

---
+++ src/main/java/beans/Idioma.java
@@ -0,0 +1,21 @@
+package beans;
+
+public class Idioma {
+
+	private  String codIdioma;
+	private String nomIdioma;
+  
+	public String getCodIdioma() {
+		return codIdioma;
+	}
+	public void setCodIdioma(String codIdioma) {
+		this.codIdioma = codIdioma;
+	}
+	public String getNomIdioma() {
+		return nomIdioma;
+	}
+	public void setNomIdioma(String nomIdioma) {
+		this.nomIdioma = nomIdioma;
+	}
+		
+}
