17ebd2629ffcaaee6826c8bbb23d1e4067ae46f8
TAV0MACA <91575347+TAV0MACA@users.noreply.github.com>
2021-11-06 09:16:45 -0500

    Create MySqlEstudiosDAO.java

---
+++ src/main/java/dao/MySqlEstudiosDAO.java
@@ -0,0 +1,69 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.MySqlConexion;
+
+import entities.Estudios;
+import interfaces.EstudiosInterfaceDAO;
+
+
+public class MySqlEstudiosDAO implements EstudiosInterfaceDAO {
+
+	@Override
+	public List<Estudios> getListEstudios() {
+		List<Estudios> listEstudios = new ArrayList<Estudios>();
+		
+		Connection cn =  null;
+		PreparedStatement psm = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			cn = MySqlConexion.getConexion();
+			
+			String sql = "select * from estudios";
+			
+			psm = cn.prepareStatement(sql);
+			rs = psm.executeQuery();
+			
+			while (rs.next()) {
+				
+				Estudios estudios = new Estudios();
+				
+				estudios.setCodEstudiante(rs.getString("CODEST"));
+				estudios.setNomEstudio(rs.getString("NOMESTUDIO"));
+				estudios.setCarrera(rs.getString("CARRERA"));
+				estudios.setTelefono(rs.getInt("TELEFONO"));
+				estudios.setCelular(rs.getInt("CELULAR"));
+				estudios.setDireccion(rs.getString("DIRECCION"));
+				estudios.setCodPais(rs.getString("CODPAIS"));
+				
+				listEstudios.add(estudios);
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			try {
+				
+				if(rs != null) rs.close();
+				if(psm != null) psm.close();
+				if(cn != null) cn.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return listEstudios;
+	}
+
+}
