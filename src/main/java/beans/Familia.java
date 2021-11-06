f31e0c03ab777fea54239ee60587d1ac176ab812
TAV0MACA <91575347+TAV0MACA@users.noreply.github.com>
2021-11-06 17:44:56 -0500

    Create Familia.java

---
+++ src/main/java/beans/Familia.java
@@ -0,0 +1,36 @@
+package beans;
+
+public class Familia {
+	
+	private String codFamilia;
+	private String nombre;
+	private String direccion;
+	private int telefono;
+	
+	public String getCodFamilia() {
+		return codFamilia;
+	}
+	public void setCodFamilia(String codFamilia) {
+		this.codFamilia = codFamilia;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+}
