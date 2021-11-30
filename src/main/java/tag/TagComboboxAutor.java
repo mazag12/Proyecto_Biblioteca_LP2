d2fa557ee28b85005a0737c55c38a1b769ca13ec
mazag12 <maxter_anime12@outlook.com>
2021-11-29 20:25:13 -0500

    AGREGAR
    CAMBIOS

---
+++ src/main/java/tag/TagComboboxAutor.java
@@ -0,0 +1,14 @@
+package tag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class TagComboboxAutor extends TagSupport{
+
+	@Override
+	public int doStartTag() throws JspException {
+		// TODO Auto-generated method stub
+		return super.doStartTag();
+	}
+	
+}
