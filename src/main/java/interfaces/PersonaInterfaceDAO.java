878682e3788fca0891e46db3cda32a37ee843731
TAV0MACA <91575347+TAV0MACA@users.noreply.github.com>
2021-12-03 17:47:04 -0500

    Update PersonaInterfaceDAO.java

--- src/main/java/interfaces/PersonaInterfaceDAO.java
+++ src/main/java/interfaces/PersonaInterfaceDAO.java
@@ -6,4 +6,8 @@ import beans.Persona;
 
 public interface PersonaInterfaceDAO {
 	public List<Persona> getListPersona();
+	public int createPersona(Persona perso);
+	public Persona getPersona(String id);
+	public int editPersona(Persona perso);
+	public int removePersona(String id);
 }
