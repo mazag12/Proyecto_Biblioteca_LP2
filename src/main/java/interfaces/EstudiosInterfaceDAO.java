fe1be85f6de39f27faef3cca89014c0d217eb9da
TAV0MACA <91575347+TAV0MACA@users.noreply.github.com>
2021-12-03 13:59:13 -0500

    Update EstudiosInterfaceDAO.java

--- src/main/java/interfaces/EstudiosInterfaceDAO.java
+++ src/main/java/interfaces/EstudiosInterfaceDAO.java
@@ -7,5 +7,8 @@ import beans.Estudios;
 public interface EstudiosInterfaceDAO {
 
 	public List<Estudios> getListEstudios();
-	
+	public int createEstudios(Estudios estu);
+	public Estudios getEstudios(String id);
+	public int editEstudios(Estudios estu);
+	public int removeEstudios(String id);
 }
