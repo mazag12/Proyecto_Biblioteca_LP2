1ac2b85039292a2e8a0a5a203d9bf95576db60ab
TAV0MACA <91575347+TAV0MACA@users.noreply.github.com>
2021-12-03 14:00:09 -0500

    Update MySqlEstudiosDAO.java

--- src/main/java/dao/MySqlEstudiosDAO.java
+++ src/main/java/dao/MySqlEstudiosDAO.java
@@ -65,5 +65,172 @@ public class MySqlEstudiosDAO implements EstudiosInterfaceDAO {
 		
 		return listEstudios;
 	}
+	
+	@Override
+	public int createEstudios(Estudios estu) {
+		int value = 0;
+		
+		Connection cn =  null;
+		PreparedStatement psm = null;
+		
+		try {
+			
+			cn = MysqlDBConexion.getConexion();
+			
+			String sql = "call SP_BIBLIOTECA_INSERTAR_ESTUDIOS(?,?,?,?,?,?)";
+			psm = cn.prepareStatement(sql);
+			psm.setString(1, estu.getNomEstudio());
+			psm.setString(2, estu.getCarrera());
+			psm.setInt(3, estu.getTelefono());
+			psm.setInt(4, estu.getCelular());
+			psm.setString(5, estu.getDireccion());
+			psm.setString(6, estu.getCodPais());
+						
+			value = psm.executeUpdate();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			try {
+				
+				if(psm != null) psm.close();
+				if(cn != null) cn.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return value;
+	}
+
+	@Override
+	public Estudios getEstudios(String id) {
+		Estudios estudios = null;
+		
+		Connection cn =  null;
+		PreparedStatement psm = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			cn = MysqlDBConexion.getConexion();
+			
+			String sql = "SELECT * FROM estudios WHERE CODEST=?";
+			
+			psm = cn.prepareStatement(sql);
+			psm.setString(1, id);
+			
+			rs = psm.executeQuery();
+			
+			if(rs.next()) {
+				
+				estudios = new Estudios();
+				estudios.setCodEstudiante(rs.getString("CODEST"));
+				estudios.setNomEstudio(rs.getString("NOMESTUDIO"));
+				estudios.setCarrera(rs.getString("CARRERA"));
+				estudios.setTelefono(rs.getInt("TELEFONO"));
+				estudios.setCelular(rs.getInt("CELULAR"));
+				estudios.setDireccion(rs.getString("DIRECCION"));
+				estudios.setCodPais(rs.getString("CODPAIS"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			try {
+				
+				if(rs != null) rs.close();
+				if(psm != null) psm.close();
+				if(cn != null) cn.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return estudios;
+	}
+
+	@Override
+	public int editEstudios(Estudios estu) {
+			int salida = 0;
+		
+		Connection cn =  null;
+		PreparedStatement psm = null;
+		
+		try {
+			
+			cn = MysqlDBConexion.getConexion();
+			
+			String sql = "UPDATE estudios SET NOMESTUDIO=upper(?), CARRERA=upper(?), TELEFONO=?, CELULAR=?, DIRECCION=upper(?), CODPAIS=upper(?)    WHERE CODEST=?";
+			psm = cn.prepareStatement(sql);
+			psm.setString(1, estu.getNomEstudio());
+			psm.setString(2, estu.getCarrera());
+			psm.setInt(3, estu.getTelefono());
+			psm.setInt(4, estu.getCelular());
+			psm.setString(5, estu.getDireccion());
+			psm.setString(6, estu.getCodPais());
+			psm.setString(7, estu.getCodEstudiante());
+			
+			salida = psm.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			try {
+				
+				if(psm != null) psm.close();
+				if(cn != null) cn.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return salida;
+	}
+
+	@Override
+	public int removeEstudios(String id) {
+		int salida = 0;
+		
+		Connection cn =  null;
+		PreparedStatement psm = null;
+		
+		try {
+			
+			cn = MysqlDBConexion.getConexion();
+			
+			String sql = "DELETE FROM estudios WHERE CODEST=?";
+			psm = cn.prepareStatement(sql);
+			psm.setString(1, id);
+			
+			salida = psm.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			try {
+				
+				if(psm != null) psm.close();
+				if(cn != null) cn.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return salida; 
+	}
 
 }
