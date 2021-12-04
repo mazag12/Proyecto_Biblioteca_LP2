031c10990befb27515802abab5fc349d2ab7b118
TAV0MACA <91575347+TAV0MACA@users.noreply.github.com>
2021-12-03 23:47:19 -0500

    Update IdiomaServlet.java

--- src/main/java/servlet/IdiomaServlet.java
+++ src/main/java/servlet/IdiomaServlet.java
@@ -36,38 +36,89 @@ public class IdiomaServlet extends HttpServlet {
     	if (type.equals("lista")) {
     		listIdioma(request, response);
     	} else if (type.equals("register")) {
-    		String codprovincia = request.getParameter("codprovincia");
-    		if(codprovincia.isEmpty()) {
-    			registerIdioma(request, response);
-    		} else {
-    			///editAutor(request, response);
-    		}
-    	} else if (type.equals("info")) {
-    		//getSubject(request, response);
+    		registerIdioma(request, response);
+    		
+    	} else if (type.equals("edit")) {
+    		editIdioma(request, response);
     	} else if (type.equals("delete")) {
-    		//deleteAutor(request, response);
+    		deleteIdioma(request, response);
     	} 
     }
     
+	protected void editIdioma(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+    	String codigo = request.getParameter("txtCodigoA");
+    	String nombre = request.getParameter("txtNombreA");
+   	
     
-    protected void listIdioma(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	DAOFactory daoFactory = DAOFactory.getFactory(DAOFactory.MYSQL);    	
+    	IdiomaInterfaceDAO dao = daoFactory.getIdioma();
+    	
+
+    		Idioma idio = new Idioma();
+    		idio.setCodIdioma(codigo);
+    		idio.setNomIdioma(nombre);
+
+    		    		
+    		int value = dao.editIdioma(idio);
+    		
+    		if (value == 1) {
+        		listIdioma(request, response);
+        	} else {
+        		//error
+        	}
+    	
+    }
+    
+	 protected void registerIdioma(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+    	String nombre = request.getParameter("txtNombre");
+  
+    	DAOFactory daoFactory = DAOFactory.getFactory(DAOFactory.MYSQL);    	
+    	IdiomaInterfaceDAO dao = daoFactory.getIdioma();
+    	
+
+    		Idioma idio = new Idioma();
+    		idio.setNomIdioma(nombre);
+
+    		int value = dao.createIdioma(idio);
+    		
+    		if (value == 1) {
+        		listIdioma(request, response);
+        	} else {
+        		//error
+        	}
+    		
+    	
+    }
+
+	
+  protected void listIdioma(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	// TODO Auto-generated method stub
     	
     	DAOFactory daoFactory = DAOFactory.getFactory(DAOFactory.MYSQL);
-    	IdiomaInterface dao = daoFactory.getIdioma();
-    	    	
-    	List<Idioma> data = dao.getListIdioma();
+    	IdiomaInterfaceDAO dao = daoFactory.getIdioma();    	
+    	List<Idioma> data = dao.getListIdiomas();
     	
     	request.setAttribute("data", data);
     	request.getRequestDispatcher("Idioma.jsp").forward(request, response);
     }
     
-    protected void registerIdioma(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	// TODO Auto-generated method stub
+  	 protected void deleteIdioma(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+		
+		String idIdioma = request.getParameter("id");
+
+    	DAOFactory daoFactory = DAOFactory.getFactory(DAOFactory.MYSQL);
+    	IdiomaInterfaceDAO dao = daoFactory.getIdioma();
     	
-    
+    	int flagResponse = dao.removeEstudios(idIdioma); // subjectModel.removeSubject(idSubject);
     	
-    }
+    	if (flagResponse == 1) {
+    		listIdioma(request, response);
+    	} else {
+    		request.setAttribute("message", "Ocurrio un problema");
+    		request.getRequestDispatcher("Idioma.jsp").forward(request, response);
+    	}
+		
+	}
     
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
