a036bcb54834d4077eaed264599315c1025ee26d
KrlCO <51756478+KrlCO@users.noreply.github.com>
2021-12-04 18:29:19 -0500

    Add files via upload
    Jose Chuquichanca

--- src/main/java/servlet/EmpleadoServlet.java
+++ src/main/java/servlet/EmpleadoServlet.java
@@ -9,8 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import beans.Autor;
 import beans.Empleado;
 import dao.DAOFactory;
+import interfaces.AutorInterface;
 import interfaces.EmpleadoInterface;
 
 /**
@@ -60,6 +62,121 @@ public class EmpleadoServlet extends HttpServlet {
     	request.setAttribute("data", data);
     	request.getRequestDispatcher("Empleado.jsp").forward(request, response);
     }
+    
+    protected void registerAutor(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+    	String name = request.getParameter("txtName");
+    	
+    	DAOFactory daoFactory = DAOFactory.getFactory(DAOFactory.MYSQL);    	
+    	EmpleadoInterface dao = daoFactory.getEmpleado();
+    	Empleado emp = dao.getEmpleado(name); 
+    	
+    	if(emp.getCodigoemple() == "SNDATA") {
+    		
+    		Empleado au = new Empleado();
+        	au.setCodigoemple("Sin ID");
+        	au.setCodperson(name);
+        	au.setCodcargo("Sin ID");
+    		
+    		int value = dao.createEmpleado(au);
+    		
+    		if (value == 1) {
+        		listEmpleado(request, response);
+        	} else {
+        		//request.setAttribute("message", "Ocurrio un problema");
+        		listEmpleado(request, response);
+        	}
+    		
+    	}else{
+    		//request.setAttribute("message", "Ocurrio un problema");
+    		listEmpleado(request, response);
+    	}
+    	
+    }
+    
+    protected void getEmpleado(HttpServletRequest request, HttpServletResponse response) 
+    		throws ServletException, IOException  {
+    	
+    	String codEmp = request.getParameter("id");
+    	
+    	DAOFactory daoFactory = DAOFactory.getFactory(DAOFactory.MYSQL);    	
+    	EmpleadoInterface dao = daoFactory.getEmpleado();
+    
+    	
+    	Empleado emp = dao.getEmpleado(codEmp); 
+    	List<Empleado> listUsuario = dao.getListEmpleado(); 
+    	request.setAttribute("usuarioData", emp);
+    	request.setAttribute("data", listUsuario);
+    	request.getRequestDispatcher("Empleado.jsp").forward(request, response);
+    }
+    
+    protected void editEmpleado(HttpServletRequest request, HttpServletResponse response) 
+    		throws ServletException, IOException  {
+    	
+    	String codemp = request.getParameter("txtcodigoempleado");
+    	String codp = request.getParameter("txtcodigoperson");
+    	String codc = request.getParameter("txtcodcargo");
+    	
+    	DAOFactory daoFactory = DAOFactory.getFactory(DAOFactory.MYSQL);    	
+    	EmpleadoInterface dao = daoFactory.getEmpleado();
+    	Empleado emp = dao.getEmpleado(codp); 
+    	    	
+    	if(emp.getCodigoemple() == "SNDATA") {
+    		Empleado au = new Empleado();
+        	au.setCodigoemple("Sin ID");
+        	au.setCodperson(codp);
+        	au.setCodcargo("Sin ID");
+    		
+	    	int flagResponde = dao.editEmpleado(au); // subjectModel.editSubject(subject);
+	    	
+	    	if (flagResponde == 1) {
+	    		listEmpleado(request, response);
+	    	} else {
+	    		//request.setAttribute("message", "Ocurrio un problema");
+	    		listEmpleado(request, response);
+	    	}
+    	}else if(emp.getCodigoemple() ==  codemp){
+    		Empleado au = new Empleado();
+        	au.setCodigoemple("Sin ID");
+        	au.setCodperson(codp);
+        	au.setCodcargo("Sin ID");
+    		
+	    	int flagResponde = dao.editEmpleado(au); // subjectModel.editSubject(subject);
+	    	
+	    	if (flagResponde == 1) {
+	    		listEmpleado(request, response);
+	    	} else {
+	    		//request.setAttribute("message", "Ocurrio un problema");
+	    		listEmpleado(request, response);
+	    	}
+	    }else {
+	    	//request.setAttribute("message", "Ocurrio un problema");
+	    	listEmpleado(request, response);
+	    }
+    	
+    }
+    
+    protected void deleteEmpleado(HttpServletRequest request, HttpServletResponse response) 
+    		throws ServletException, IOException  {
+    	
+    	String codigo = request.getParameter("id");
+    	
+    	DAOFactory daoFactory = DAOFactory.getFactory(DAOFactory.MYSQL);    	
+    	EmpleadoInterface dao = daoFactory.getEmpleado();
+    	
+    	
+    	int flagResponse = dao.removeEmpleado(codigo); // subjectModel.removeSubject(idSubject);
+    	
+    	if (flagResponse == 1) {
+    		listEmpleado(request, response);
+    	} else {
+    		//request.setAttribute("message", "Ocurrio un problema");
+    		listEmpleado(request, response);
+    	}
+    	
+    }
+    
+    
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
